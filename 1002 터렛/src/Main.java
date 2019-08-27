dec6c555719c3980fcb3e4fb4b7a6b680a1eabbb
YOONSUNGHYEON <qead753951@naver.com>
2019-08-27 22:51:50 +0900

    completed1002

--- 1002 터렛/src/Main.java
+++ 1002 터렛/src/Main.java
@@ -9,11 +9,10 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		int testNum= Integer.parseInt(reader.readLine());
-		int x1 = 0, y1 = 0, r1 = 0,x2 = 0, y2 = 0, r2=0;
+		int testNum = Integer.parseInt(reader.readLine());
+		int x1 = 0, y1 = 0, r1 = 0, x2 = 0, y2 = 0, r2 = 0;
 		int[] arr = new int[testNum];
-		for(int i=0; i<testNum; i++)
-		{
+		for (int i = 0; i < testNum; i++) {
 			StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
 			x1 = Integer.parseInt(tokenizer.nextToken());
 			y1 = Integer.parseInt(tokenizer.nextToken());
@@ -22,28 +21,45 @@ public class Main {
 			y2 = Integer.parseInt(tokenizer.nextToken());
 			r2 = Integer.parseInt(tokenizer.nextToken());
 
-
-		double num = Math.sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
-		int R  = r1+ r2;
-		if(R > num) {
-
-		}
-		else if(R == num)
-		{
-			if(R == 0)
+			double R = Math.sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
+			 if (r1 > r2) {
+		    	  int temp = r1;
+		    	  r1 = r2;
+		    	  r2 = temp;
+		    	  }
+			if (R > r2 )
 			{
+				if(r1+r2<R)
+					arr[i] = 0;
+				else if(r1+r2 == R)
+					arr[i] = 1;
+				else if(r1+r2 > R)
+					arr[i] = 2;
 
 			}
-		}
-		else if(R > num)
-		{
+			else if (R <= r2 && R>0)
+			{
+				if(r2>R+r1)
+					arr[i] = 0;
+				else if(r2 == R+r1)
+					arr[i] = 1;
+				else if(r2 < R+r1)
+					arr[i] = 2;
+			}
+			else if (R ==0)
+			{
+				if(r2!=r1)
+					arr[i] = 0;
+				else if(r2==r1)
+					arr[i] = -1;
+			}
 
 		}
-		else
-		{
 
+		for(int i = 0; i < testNum; i++)
+		{
+			System.out.println(arr[i]);
 		}
+
 	}
-
-}
 }
