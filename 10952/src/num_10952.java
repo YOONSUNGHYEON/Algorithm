364ffd58668aa15d5b54d56ea6994ed528de444b
YOONSUNGHYEON <qead753951@naver.com>
2020-06-03 02:17:49 +0900

    10952

---
+++ 10952/src/num_10952.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class num_10952 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A;
+		int B;
+		while ((A = sc.nextInt()) != 0 && (B = sc.nextInt()) != 0) {
+			System.out.println(A + B);
+		}
+	}
+
+}
